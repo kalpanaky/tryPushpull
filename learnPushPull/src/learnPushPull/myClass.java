eb20a85de1f155efbbc5f2277e184693e39a2c46
Kalpana Yadav <kalpana.yadav@gmail.com>
2018-04-08 08:27:17 -0500

    I have changed it again

--- learnPushPull/src/learnPushPull/myClass.java
+++ learnPushPull/src/learnPushPull/myClass.java
@@ -5,7 +5,7 @@ public class myClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("hello lala learning changing in remote");
+		System.out.println("hello my project lala learning changing in remote");
 
 	}
 
